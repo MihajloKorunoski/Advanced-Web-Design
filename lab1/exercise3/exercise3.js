bf4f6ec3fd83736555d93bb9b9616a7e84f47441
Mihaylo Markov Korunoski <mihajlo_korunoski@icloud.com>
2024-11-21 03:56:15 +0100

    Refactoring Part 2

--- lab1/exercise3/exercise3.js
+++ lab1/exercise3/exercise3.js
@@ -1,20 +1,3 @@
-//Креирај функција, која на влез ќе прима две низи составени од стрингови, кои претставуваат состав на два сендвича. Целта на оваа функција е да спореди дали сендвичите се направени од ист тип на леб, така што лебот е секогаш поставен на почетокот и крајот на низата.
-//
-// Пример како треба да изгледа функцијата:
-//
-// hasSameBread(
-//   ["white bread", "lettuce", "white bread"],
-//   ["white bread", "tomato", "white bread"]
-// ) ➞ true
-//
-// For example:
-//
-// Input	Result
-// white bread, lettuce, white bread
-// white bread, tomato, white bread
-// true
-
-
 'use strict'
 
 let readline = require('readline');
